96678b13e7082468dbdcd402acb320c6b533a9e9
kgryte <kgryte@gmail.com>
2016-02-24 03:06:05 -0800

    Add check for NaN and add associated tests

--- test/test.js
+++ test/test.js
@@ -12,3 +12,15 @@ tape( 'main export is a function', function test( t ) {
 	t.equal( typeof nextafter, 'function', 'main export is a function' );
 	t.end();
 });
+
+tape( 'if `x` is `NaN`, the function returns `NaN`', function test( t ) {
+	var z = nextafter( NaN, 5.0 );
+	t.ok( z !== z, 'returns NaN' );
+	t.end();
+});
+
+tape( 'if `y` is `NaN`, the function returns `NaN`', function test( t ) {
+	var z = nextafter( 5.0, NaN );
+	t.ok( z !== z, 'returns NaN' );
+	t.end();
+});
